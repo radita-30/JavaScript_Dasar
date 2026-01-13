285c08a6ecec94e3e63bb20d048b5edb7a134ccf
radita-30 <raditapyol@gmail.com>
2026-01-13 12:16:52 +0700

    Refactor arithmetic and logical operation handlers

--- script16.js
+++ script16.js
@@ -1,29 +1,30 @@
-let aritmatikaAngka1, operatorAritmatika,AritmatikaAngka2,HasilOperatorAritmatika;
+let aritmatikaAngka1,operatorAritmatika,artimatikaAngka2,hasilOperatorAritmatika;
 
 document.getElementById('buttonAritmatika').onclick = function(){
-    // input
-    aritmatikaAngka1 = document.getElementById('aritmatikaAngka1').value;
-    aritmatikaAngka2 = document.getElementById('aritmatikaAngka2').valueAsNumber;
-    operatorAritmatika = document.getElementById('operatorAritmatika').value;
-    
-    // operasi
-    hasilOperatorAritmatika = eval(aritmatikaAngka1 + operatorAritmatika + aritmatikaAngka2);
+	// input
+	aritmatikaAngka1 = document.getElementById('aritmatikaAngka1').value;
+	aritmatikaAngka2 = document.getElementById('aritmatikaAngka2').valueAsNumber;
+	operatorAritmatika = document.getElementById('operatorAritmatika').value;
+	
+	// operasi
+	hasilOperatorAritmatika = eval(aritmatikaAngka1 + operatorAritmatika + aritmatikaAngka2);
+	
+	// output
+	document.getElementById("hasilOperatorAritmatika").textContent = hasilOperatorAritmatika;
 
-    // output
-    document.getElementById("hasilOperatorAritmatika").textContent = hasilOperatorAritmatika;
-}
+};
 
-let boolean1, boolean2,hasilOperatorLogika,OperatorLogika;
-
+let boolean1,boolean2,hasilOperatorLogika,operatorLogika;
 document.getElementById('buttonLogika').onclick = function(){
-    // input
-    boolean1 = document.getElementById('boolean1').value;
-    boolean2 = document.getElementById('boolean2').valueAsNumber;
-    operatorLogika = document.getElementById('operatorLogika').value;
-    
-    // operasi
-    hasilOperatorLogika = eval(boolean1 + operatorLogika + boolean2);
+	// input
+	boolean1 = document.getElementById('boolean1').value;
+	boolean2 = document.getElementById('boolean2').valueAsNumber;
+	operatorLogika = document.getElementById('operatorLogika').value;
+	
+	// operasi
+	hasilOperatorLogika = eval(boolean1 + operatorLogika + boolean2);
+	
+	// output
+	document.getElementById("hasilOperatorLogika").textContent = hasilOperatorLogika;
 
-    // output
-    document.getElementById("hasilOperatorLogika").textContent = hasilOperatorLogika;
-}
+};
