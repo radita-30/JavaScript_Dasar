07bf63f402d384a2a1e6ca316777737dc87b2f93
radita-30 <raditapyol@gmail.com>
2026-01-13 11:49:40 +0700

    Add files via upload

---
+++ script04.js
@@ -0,0 +1,46 @@
+// jadi ini adalah komen, jadi bebas aja
+
+// 1. variabel dengan let
+let nama =  "Ucup Surucup";
+
+// tampilkan data
+console.log(nama);
+
+// kita ubah nilai variabel nama
+nama = "Otong Surotong";
+console.log(nama);
+
+// 2. variabel dengan var
+var namaDepan = "Ucup";
+console.log(namaDepan);
+namaDepan = "Otong";
+console.log(namaDepan);
+
+// kelakukan dari let
+let namaBelakang = "Surucup"
+{
+        let namaBelakang = "Surotong"
+        console.log(namaBelakang);
+}
+console.log(namaBelakang);
+
+// kelakukan dari var
+// var namaTengah = "keren"
+{
+        var namaTengah = "Ganteng";
+        console.log(namaTengah);
+}
+console.log(namaTengah);
+
+// kasus khusus tanpa keyword akan jadi var
+contohBaru = "Bala-bala";
+{
+    gorengan = "Combro";
+}
+console.log(gorengan);
+
+// 3. conts
+
+const TTL = "10 Maret 2022"
+console.log(TTL);
+// TTL = "11 Maret 2054"; gak boleh dilakukan karna sudah janji konstan
---
+++ script17.js
@@ -0,0 +1,11 @@
+let nama = "otong";
+
+console.log("nama" + " = " + nama);
+
+if(nama==="otong"){
+    console.log("Haloo otoong");
+}else{
+    console.log("au ah bukan otong, aku sebel sama kamu");
+} 
+
+console.log("akhir dari program")
